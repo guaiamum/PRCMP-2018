5a241562310e04dfecff5019f2888d943468f499
JPTL <44479879+JPTL@users.noreply.github.com>
2018-11-02 19:42:29 +0000

    Update command.sh

--- SPRINT2/1/command.sh
+++ SPRINT2/1/command.sh
@@ -1,6 +1,7 @@
 first=3; last=6; cat dados.txt | tail -n $first | head -n $((last-first))
 mkdir ~/temp; cp [bB]*.txt ~/temp
 
-Resolução (João)
 a)
-cat dados.txt | tail +3 | head -4 > texto.txt
+tail +3 dados.txt | head -4 >> texto.txt
+
+b)
