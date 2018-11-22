db7780d4e6488e36b65ceee1811cd52f81765ac2
JPTL <44479879+JPTL@users.noreply.github.com>
2018-11-22 18:31:13 +0000

    added cut command to print name and NIB

--- SPRINT3/1/a.1.sh
+++ SPRINT3/1/a.1.sh
@@ -5,6 +5,6 @@ OUTPUTFILE=output.txt
 regex=";S;(.*;003)"
 
 while read line; do
-    if [[ $line =~ $regex ]]; then echo $line >> $OUTPUTFILE; fi
+    if [[ $line =~ $regex ]]; then echo $line| cut -d";" -f1,4 >> $OUTPUTFILE; fi
 done < $FILE
     
