b80ce123e49018c0bfdcb48e7105fddc3ba6d9e0
Cainã Brazil <guaiamum@CainasHotRetina.local>
2018-11-23 20:02:08 +0000

    identation and ignoring case

--- SPRINT3/1/a.1.sh
+++ SPRINT3/1/a.1.sh
@@ -5,6 +5,8 @@ OUTPUTFILE=output.txt
 regex=";S;(.*;003)"
 
 while read line; do
-    if [[ $line =~ $regex ]]; then echo $line| cut -d";" -f1,4 >> $OUTPUTFILE; fi
+    if [[ $line =~ $regex ]]; then
+        echo $line| cut -d";" -f1,4 >> $OUTPUTFILE;
+    fi
 done < $FILE
     
--- SPRINT3/1/b.sh
+++ SPRINT3/1/b.sh
@@ -5,7 +5,7 @@ FILE=TopClientes.txt
 # Iterating over arguments
 for arg in "$@"
 do
-    grep "$arg"$ $FILE | echo "Total of clients for $arg: " $(gwc -l)
+    grep -i $arg$ $FILE | echo "Total of clients for $arg: " $(gwc -l)
 done
 
 exit 0
